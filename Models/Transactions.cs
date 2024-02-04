25767c902496d177954493646d8dd0e0a6c35312
Emmanuel Sansberro <36830673+sansemmanuel@users.noreply.github.com>
2024-02-04 17:50:44 -0300

    models

---
+++ Models/Transactions.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace expensesapp.Models
+{
+    public class Transactions
+    {
+        [Key]
+        public int TransactionId { get; set; }   
+       //Cat ID
+       public int CategoryId { get; set; }  
+        
+        public int Amount { get; set; } 
+        public string? Notes { get; set; }   
+        public DateTime Date {  get; set; } = DateTime.UtcNow;
+
+    }
+}
