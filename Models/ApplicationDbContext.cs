c69917f6e0148680e3fe63fbca89401ad5ce6a65
Emmanuel Sansberro <36830673+sansemmanuel@users.noreply.github.com>
2024-02-05 01:41:33 -0300

    54545

--- Models/ApplicationDbContext.cs
+++ Models/ApplicationDbContext.cs
@@ -1,15 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System.Transactions;
-
 namespace expensesapp.Models
 {
     public class ApplicationDbContext:DbContext
     {
     public ApplicationDbContext(DbContextOptions options):base(options)
-        {
-
-        }
-        public DbSet<Transactions> Transactions { get; set; }
+        {}
+        public DbSet<Transaction> Transactions { get; set; }
         public DbSet<Category> Categories{ get; set; }
 
     }
--- Models/Transactions.cs
+++ Models/Transactions.cs
@@ -3,18 +3,41 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace expensesapp.Models
 {
-    public class Transactions
+    public class Transaction
     {
-        
+
         [Key]
-        public int TransactionId { get; set; }   
-       //Cat ID
-       public int CategoryId { get; set; }  
-        public Category Category { get; set; }
-        public int Amount { get; set; }
-        [Column(TypeName ="nvarchar(75)")]
-        public string? Notes { get; set; }   
-        public DateTime Date {  get; set; } = DateTime.UtcNow;
+        public int TransactionId { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Please select a category.")]
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+
+        [Range(0.01, double.MaxValue, ErrorMessage = "Amount should be greater than 0.")]
+        public decimal Amount { get; set; }
+
+        [Column(TypeName = "nvarchar(75)")]
+        public string? Note { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        [NotMapped]
+        public string? CategoryTitleWithIcon
+        {
+            get
+            {
+                return Category == null ? "" : Category.Icon + " " + Category.Title;
+            }
+        }
+
+        [NotMapped]
+        public string? FormattedAmount
+        {
+            get
+            {
+                return ((Category == null || Category.Type == "Expense") ? "- " : "+ ") + Amount.ToString("C0");
+            }
+        }
 
     }
 }
