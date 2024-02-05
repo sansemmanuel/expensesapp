a7c293494f783291353190ac2a7220acc3cd83c6
Emmanuel Sansberro <36830673+sansemmanuel@users.noreply.github.com>
2024-02-05 14:28:40 -0300

    billie jean its not my lovvvvvr

--- obj/Debug/net8.0/expensesapp.AssemblyInfo.cs
+++ obj/Debug/net8.0/expensesapp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("expensesapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5df32c75bdd18fe8203148e95b01d20575fefcd8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1464104caa3d947439071e5472ad144df323a4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("expensesapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("expensesapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
