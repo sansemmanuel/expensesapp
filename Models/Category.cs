a9828c07e2a258281fa72bf0204a4e80004c1a41
Emmanuel Sansberro <36830673+sansemmanuel@users.noreply.github.com>
2024-02-06 18:51:04 -0300

    icon

--- Models/Category.cs
+++ Models/Category.cs
@@ -15,5 +15,13 @@ namespace expensesapp.Models
         [Column(TypeName = "nvarchar(10)")]
         public string Type { get; set; } = "Expense";
 
+        [NotMapped]
+        public string TitleWithIcon
+        {
+            get
+            {
+                return this.Icon + " " + this.Title;
+            }
+        }
     }
 }
