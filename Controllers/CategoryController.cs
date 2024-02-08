443e66cee8912dcfc10ddd886007c02cf2119f6b
Emmanuel Sansberro <36830673+sansemmanuel@users.noreply.github.com>
2024-02-08 00:04:47 -0300

    getdetails/5deleted

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -24,23 +24,6 @@ namespace expensesapp.Controllers
             return View(await _context.Categories.ToListAsync());
         }
 
-        // GET: Category/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.CategoryId == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
 
         // GET: Category/Create
         public IActionResult AddOrEdit(int id=0)
