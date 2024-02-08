3e88fafcd15cbe02a8d4b34011605ad67396b013
Emmanuel Sansberro <36830673+sansemmanuel@users.noreply.github.com>
2024-02-08 02:28:45 -0300

    deleteactionform

--- obj/Debug/net8.0/expensesapp.AssemblyInfo.cs
+++ obj/Debug/net8.0/expensesapp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("expensesapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+443e66cee8912dcfc10ddd886007c02cf2119f6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91662e97559e9a121869b23b09f34cd6f9cd0345")]
 [assembly: System.Reflection.AssemblyProductAttribute("expensesapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("expensesapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
