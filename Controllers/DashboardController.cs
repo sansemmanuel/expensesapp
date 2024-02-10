3692f9f1df0cdadc8907a859a309a84935098e00
Emmanuel Sansberro <36830673+sansemmanuel@users.noreply.github.com>
2024-02-10 19:34:49 -0300

    linechart

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -1,6 +1,8 @@
 ﻿using expensesapp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Syncfusion.EJ2.Charts;
+using System.Globalization;
 
 namespace expensesapp.Controllers
 {
@@ -8,7 +10,7 @@ namespace expensesapp.Controllers
     {
         private readonly ApplicationDbContext _context;
 
-        public DashboardController (ApplicationDbContext context)
+        public DashboardController(ApplicationDbContext context)
 
         {
             _context = context;
@@ -19,19 +21,80 @@ namespace expensesapp.Controllers
             DateTime StartDate = DateTime.Today.AddDays(-6);
             DateTime EndDate = DateTime.Today;
             List<Transaction> SelectedTransactions = await _context.Transactions
-            
-            .Include(y => y.Date >= StartDate && y.Date <= EndDate)
+
+          .Include(x => x.Category)
             .Where(y => y.Date >= StartDate && y.Date <= EndDate)
             .ToListAsync();
-            
-            
+
+            //TOTAL INCOME
+            int TotalIncome = SelectedTransactions
+                .Where(i => i.Category.Type == "Income")
+                .Sum(j => j.Amount);
+            ViewBag.TotalIncome = TotalIncome.ToString("C0");
+            //TOTAL EXPENSE
+            int TotalExpense = SelectedTransactions
+                .Where(i => i.Category.Type == "Expense")
+                .Sum(j => j.Amount);
+            ViewBag.TotalExpense = TotalExpense.ToString("C0");
+            //BALANCE 
+            int Balance = TotalIncome - TotalExpense;
+            CultureInfo culture = CultureInfo.CreateSpecificCulture("en-US");
+            culture.NumberFormat.CurrencyNegativePattern = 1;
+            ViewBag.Balance = String.Format(culture, " {0:C0}", Balance);
+            //donut chart -exp by category
+            ViewBag.DonutChartData = SelectedTransactions
+                .Where(i => i.Category.Type
+                == "Expense")
+                .GroupBy(j => j.Category.CategoryId)
+                .Select(k => new
+                {
+                    categoryTitleWithIcon = k.First().Category.Icon + " " + k.First().Category.Title,
+                    amount = k.Sum(j => j.Amount),
+                    formattedAmount = k.Sum(j => j.Amount).ToString("C0"),
+                })
+            .OrderByDescending(l => l.amount);
+
+
+
+
+            //SplineType Chart - TotalIncome vs expense
+            //income
+            List<SplineChartData> IncomeSummary = SelectedTransactions
+                .Where(i => i.Category.Type == "Income")
+                .GroupBy(j => j.Date)
+                .Select(k => new SplineChartData()
+                {
+                    day = k.First().Date.ToString("dd-MMM"),
+                    income = k.Sum(l => l.Amount)
+                })
+                           .ToList();
+
+
+            //EXPENSE SUMM
+            List<SplineChartData> ExpenseSummary = SelectedTransactions
+                .Where(i => i.Category.Type == "Expense")
+                .GroupBy(j => j.Date)
+                .Select(k => new SplineChartData()
+                {
+                    day = k.First().Date.ToString("dd-MMM"),
+                    expense = k.Sum(l => l.Amount)
+                })
+
+
+           .ToList();
+
             return View();
-        
-        
+
         }
-   
-    
-    
-    
+
+
+
+
+    }
+    public class SplineChartData
+    {
+        public string day;
+        public int income;
+        public int expense;
     }
 }
