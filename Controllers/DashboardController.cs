85ff41794f3f43e17329e9eb3298ea7de11e3d7b
Emmanuel Sansberro <36830673+sansemmanuel@users.noreply.github.com>
2024-02-10 13:58:26 -0300

    dashcontroller

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -1,12 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using expensesapp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace expensesapp.Controllers
 {
     public class DashboardController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public DashboardController (ApplicationDbContext context)
+
         {
-            return View();
+            _context = context;
         }
+        public async Task<ActionResult> Index()
+        {
+            //last seven days
+            DateTime StartDate = DateTime.Today.AddDays(-6);
+            DateTime EndDate = DateTime.Today;
+            List<Transaction> SelectedTransactions = await _context.Transactions
+            
+            .Include(y => y.Date >= StartDate && y.Date <= EndDate)
+            .Where(y => y.Date >= StartDate && y.Date <= EndDate)
+            .ToListAsync();
+            
+            
+            return View();
+        
+        
+        }
+   
+    
+    
+    
     }
 }
