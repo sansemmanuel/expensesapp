1355bd75567e9e378926a6c7149836fd73df7125
Emmanuel Sansberro <36830673+sansemmanuel@users.noreply.github.com>
2024-02-11 02:26:37 -0300

    as

--- obj/Debug/net8.0/expensesapp.AssemblyInfo.cs
+++ obj/Debug/net8.0/expensesapp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("expensesapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+858186cb6afa8acc7f649c2ad9aede8b5e32db7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("expensesapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("expensesapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
